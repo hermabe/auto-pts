a24bdee2dfc43592f6e8f16b58d2ee34f1f460f0
Berget, Herman <herman.berget@nordicsemi.no>
2021-10-29 10:32:08 +0200

    wid: Add support for GATT Wid 153
    WID 153: Please write to client support feature handle = '0015'O to
    enable EATT and Robust Caching. Discover all characteristics if needed.
    
    Affects GATT/CL/GPM/BV-11-C

--- wid/gatt.py
+++ wid/gatt.py
@@ -1805,6 +1805,20 @@ def hdl_wid_152(desc):
 
     return False
 
+def hdl_wid_153(desc):
+    """
+    Please write to client support feature handle = '0015'O to enable EATT and Robust Caching.
+    Discover all characteristics if needed.
+    """
+    MMI.reset()
+    MMI.parse_description(desc)
+
+    hdl = MMI.args[0]
+
+    btp.gattc_write(btp.pts_addr_type_get(), btp.pts_addr_get(), hdl, '01')
+
+    return True
+
 
 def hdl_wid_304(desc):
     MMI.reset()
