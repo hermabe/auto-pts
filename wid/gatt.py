ff1f13946980130473f9678ea9cc53546572c1e1
Berget, Herman <herman.berget@nordicsemi.no>
2021-11-12 10:29:36 +0100

    wid: Add support for GATT wid 161
    WID 161: Please send Read request on ATT over LE using handle 'XXXX'O
    
    Affects GATT/CL/GPM/BV-11-C

--- wid/gatt.py
+++ wid/gatt.py
@@ -1848,6 +1848,18 @@ def hdl_wid_153(desc):
     return True
 
 
+def hdl_wid_161(desc):
+    """Please send Read request on ATT over LE using handle 'XXXX'O"""
+    MMI.reset()
+    MMI.parse_description(desc)
+
+    hdl = MMI.args[0]
+
+    btp.gattc_read(btp.pts_addr_type_get(), btp.pts_addr_get(), hdl)
+
+    return True
+
+
 def hdl_wid_165(desc):
     """
     Please delete the bond if IUT was bonded previously.
